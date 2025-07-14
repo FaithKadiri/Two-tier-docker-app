763138ed7baf0fdad769f9b80b97e3668fdb9bd8
FaithKadiri <149117977+FaithKadiri@users.noreply.github.com>
2025-07-14 09:36:08 +0100

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/api')
-def api():
-    return 'Hello from the Backend!'
+@app.route("/")
+def home():
+    return render_template("index.html")
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5010)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
 
